9f931fd57c0c75dca2b44bd23391b3ff90c97047
Fikri <fikriihsan003@yahoo.com>
2016-04-26 19:44:11 +0700

    menambahkan data diri,SRC,SS

---
+++ src/Asp.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author user
+ */
+public class Asp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Retailer n =new Retailer("fikri","124893","Retailer",1000,2);
+        n.display();
+        
+        wholeSaler m=new wholeSaler("ihsan","2222","distributor",12000,11000);
+        m.show();
+    }
+    
+}
